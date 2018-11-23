eb358f1d1604c8f0c5cbc8b863f5ec2fffa96a4f
luowangchang <1648942381@qq.com>
2018-11-23 15:18:29 +0800

    使用pagehelper插件实现分页查询

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -4,6 +4,9 @@ import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
+import com.e3mall.common.pojo.EasyUIDataGridResult;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +27,23 @@ public class ItemServiceImpl implements ItemService {
         criteria.andIdEqualTo(itemId);
         List<TbItem> tbItems =itemMapper.selectByExample(example);
         return tbItems!=null&&tbItems.size()>0?tbItems.get(0):null;
+    }
+
+    @Override
+    public EasyUIDataGridResult getItemList(int page, int rows) {
+        //设置分页信息
+        PageHelper.startPage(page,rows);
+        //执行查询
+        TbItemExample example = new TbItemExample();
+        List<TbItem> tbItems = itemMapper.selectByExample(example);
+        //创建返回值对象
+        EasyUIDataGridResult result = new EasyUIDataGridResult();
+        result.setRows(tbItems);
+        //取分页结果
+        PageInfo<TbItem> pageInfo=new PageInfo<>(tbItems);
+        //取总记录数
+        long total=pageInfo.getTotal();
+        result.setTotal(total);
+        return result;
     }
 }
