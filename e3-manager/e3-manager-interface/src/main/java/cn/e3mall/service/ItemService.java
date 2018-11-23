caec25e5a397f454b4d25eedd69136264d839d52
luowangchang <1648942381@qq.com>
2018-11-23 15:18:29 +0800

    使用pagehelper插件实现分页查询

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,7 +1,9 @@
 package cn.e3mall.service;
 
 import cn.e3mall.pojo.TbItem;
+import com.e3mall.common.pojo.EasyUIDataGridResult;
 
 public interface ItemService {
     TbItem getItemById(long itemId);
+    EasyUIDataGridResult getItemList(int page,int rows);
 }
